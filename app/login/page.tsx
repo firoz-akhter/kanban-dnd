0ee58ec858778650c1a623c24ae87781851730b8
Firoz Akhter <firozakhter789@gmail.comgit config --list>
2025-12-18 22:52:26 +0530

    minor changes const baseUrl addition

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,12 +10,14 @@ const Login = () => {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
+  const baseUrl = "http://localhost:4000/api";
+
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:4000/api/auth/login", {
+      const response = await fetch(`${baseUrl}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(credentials),
